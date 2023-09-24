a3e6e340804bf47ee0900ebaea9151e5f96f3558
kinba09 <abnikahilasamy@gmail.com>
2023-09-24 16:25:00 +0530

    inital commit mlapi.py

---
+++ mlapi.py
@@ -0,0 +1,47 @@
+# Libraries
+from fastapi import FastAPI
+import pickle
+import numpy as np 
+import requests 
+import gdown
+
+# Custom Files 
+from CustomData import *
+from encoding import *
+
+# Initializing 
+app = FastAPI()
+
+pickled_model = pickle.load(open('modeltest.pkl', 'rb'))
+
+
+def pipeline(item: InputData) -> EncodedData:
+    return np.array([
+                    Gender_encoding[item.Gender], \
+                    Married_encoding[item.Married], \
+                    Dependents_encoding[item.Dependents], \
+                    Education_encoding[item.Education],\
+                    Self_Employed_encoding[item.Self_Employed],\
+                    item.LoanAmount, \
+                    item.Credit_History, \
+                    Property_Area_encoding[item.Property_Area], \
+                    item.Total_Income, \
+                    item.Loan_Term_years
+                     ])
+
+@app.get('/')
+async def scoring_endpoint(item : InputData):
+    newData = pipeline(item)
+    newData = newData.reshape((1, -1))
+    yhat = pickled_model.predict(newData)[0]
+    #yhat = round(yhat, 2)
+    if yhat == 0:
+        x = 'Yes your loan can be approved !'
+        Ballons_bool = True
+    else:
+        x = 'Sorry, Your loan can not be approved'
+        Ballons_bool = False
+    # pred_text = str(type(yhat))
+    pred_text = x
+    return {'prediction' : pred_text,\
+            'Ballons' : Ballons_bool} 
